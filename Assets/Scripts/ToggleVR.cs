88dbdea5b04ed13fd8c01dde6c36d53ae5bece15
Sanjit Singh <sanjitpsingh@yahoo.com>
2018-03-31 14:11:01 -0400

    Fixed File Structure

---
+++ Assets/Scripts/ToggleVR.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.XR;
+
+public class ToggleVR : MonoBehaviour
+{
+	//Example of toggling VRSettings
+	private void Update ()
+	{
+		//If V is pressed, toggle VRSettings.enabled
+		if (Input.GetKeyDown(KeyCode.V))
+		{
+			UnityEngine.XR.XRSettings.enabled = !UnityEngine.XR.XRSettings.enabled;
+			Debug.Log("Changed VRSettings.enabled to:"+UnityEngine.XR.XRSettings.enabled);
+		}
+	}
+}
