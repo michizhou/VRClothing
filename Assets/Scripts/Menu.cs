0737c5e13869714e9f491ec84ec86945fb1937ad
Sanjit Singh <sanjitpsingh@yahoo.com>
2018-03-31 11:54:01 -0400

    Oculus SDK added

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+	//OnClick listener for UI
+	public void ChangeScene(string scene)
+	{
+		Application.LoadLevel(scene);
+	}
+}
