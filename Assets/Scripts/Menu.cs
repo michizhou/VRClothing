a8b6b4a2c5b451f027b111de88f07e3641bb673f
Sanjit Singh <sanjitpsingh@yahoo.com>
2018-03-31 20:32:26 -0400

    Modified leap Motion scripts, got VR working, modifed scenes, prefabs and assets

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.XR;
 
 public class Menu : MonoBehaviour {
 
 	//OnClick listener for UI
 	public void ChangeScene(string scene)
 	{
-		Application.LoadLevel(scene);
+ 		Application.LoadLevel(scene);
+
 	}
 }
